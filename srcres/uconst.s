257c6baa22ebebf1582c13e340ce1616818891c3
bc <costin.botescu@gmail.com>
2022-01-03 09:10:09 +0200

    @uconst

--- srcres/uconst.s
+++ srcres/uconst.s
@@ -10,5 +10,17 @@ function uconst_add(sd s,sd sz)
 	call uconst_spin(fls#,s,sz,(TRUE))
 endfunction
 
-function uconst_spin(sd *f,sd *s,sd *sz,sd *is_new)
+function uconst_spin(sd f,sd s,sd sz,sd is_new)
+	add f (size_cont_top)
+	sd found
+	setcall found uconst_search(f,s,sz,is_new)
+	if found==(FALSE)
+		add f (size_cont_top)
+		call uconst_search(f,s,sz,is_new)
+	endif
+endfunction
+
+#b
+function uconst_search(sd *f,sd *s,sd *sz,sd *is_new)
+	return (FALSE)
 endfunction
