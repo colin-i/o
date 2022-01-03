1ce38b2c893019ac7474697d34a3778514b83100
bc <costin.botescu@gmail.com>
2022-01-03 14:00:10 +0200

    plans at uconst

--- srcres/uconst.s
+++ srcres/uconst.s
@@ -12,15 +12,26 @@ endfunction
 
 function uconst_spin(sd f,sd s,sd sz,sd is_new)
 	add f (size_cont_top)
+	#search in includes declared in other logs
 	sd found
-	setcall found uconst_search(f,s,sz,is_new)
+	setcall found uconst_search(f,s,sz,(FALSE))
 	if found==(FALSE)
+		#search in includes declared this log
 		add f (size_cont_top)
 		call uconst_search(f,s,sz,is_new)
 	endif
+	#search in constants declared in this file, with respect to is_new
 endfunction
 
 #b
-function uconst_search(sd *f,sd *s,sd *sz,sd *is_new)
+function uconst_search(sv fs,sd *s,sd *sz,sd *is_new)
+#	sd cursor
+#	set cursor fs#
+	add fs :
+#	set fs fs#d^
+#	add fs cursor
+#	while cursor!=fs
+#		add cursor (dword)
+#	endwhile
 	return (FALSE)
 endfunction
