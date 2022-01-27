d040f6e8b7fb2a79f1d85474ff97653cecf3f9bf
bc <costin.botescu@gmail.com>
2022-01-27 10:47:05 +0200

    adapting according to bugs

--- src/linux/obj.s
+++ src/linux/obj.s
@@ -6,9 +6,12 @@ Format ElfObj
 Include "./files/xcomimports.h"
 
 Function Message(str text)
-	data stderrorobject_init^stderr
-	data stderrorobject#1
-	set stderrorobject stderrorobject_init#
+#binutils-x86-64-linux-gnu:amd64 (2.37.50.20220106-2ubuntu1, 2.37.90.20220126-0ubuntu1)
+#ld will take stderr in data through rel.dyn only with another presence in text
+#	data stderrorobject_init^stderr
+#	data stderrorobject#1
+	sd stderrorobject^stderr
+	set stderrorobject stderrorobject#
 
 	Chars visiblemessagedata={0x0a,0}
 	Str visiblemessage^visiblemessagedata
