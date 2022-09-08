dc82e746eacbce327b98e3356de2984d8a44e8ba
bc <costin.botescu@gmail.com>
2022-09-08 15:32:15 +0300

    info

--- src/files/actions/main/index.s
+++ src/files/actions/main/index.s
@@ -84,7 +84,7 @@ if loop==1
 					if parses==(pass_init)
 						#tested at function gather; FORMAT is here starting with FUNCTIONX to set the mask knowing the format
 						if formatdefined==0;Set formatdefined 1;endif
-						if commandset==(cDECLARE)        #needing to find virtual start
+						if commandset==(cDECLARE)        #needing to find virtual start, at object is difficult, there is no virtual, ostrip will use: ld...-Tdata calculated, objcopy --update-section, write memsize, ounused will include virtual point
 						elseIf commandset==(cIMPORTLINK) #needing importx here
 						elseif commandset==(cSTARTFUNCTION);elseif commandset==(cENDFUNCTION)
 						ElseIf commandset==(cLIBRARY)
