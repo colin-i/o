d31ce40a719104b92627454680bea289adf7f4c9
bc <costin.botescu@gmail.com>
2022-10-03 07:57:26 +0300

    small start

--- ostrip/ostrip.s
+++ ostrip/ostrip.s
@@ -11,5 +11,16 @@ format elfobj64
 #these are not position independent code and inplace relocs add better with obj64 but at obj32 use addend>=0 or sum<0 error check
 
 #at shared: pointers
-#	.rela.dyn addends from pointers to data section
+#	.rela.dyn:
+#		addends from pointers to data section (this and the previous are saying the same thing but maybe is compatibility)
 #		data section offsets (direct:printf, pointers to text/data sections)
+
+const EXIT_SUCCESS=0
+const EXIT_FAILURE=1
+
+entrylinux main(sd argc,ss *argv0,ss *exec,ss *obj1,ss *log1)   #... objN logN
+
+if argc>(1+3)  #0 is all the time
+	return (EXIT_SUCCESS)
+endif
+return (EXIT_FAILURE)
