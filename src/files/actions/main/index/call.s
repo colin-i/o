b9050979ef0dfbb8b47fa788b412e112e7617ba9
bc <costin.botescu@gmail.com>
2022-10-26 16:38:02 +0300

    info

--- src/files/actions/main/index/call.s
+++ src/files/actions/main/index/call.s
@@ -1,4 +1,4 @@
 
 
 Data callfn=callfunction
-SetCall errormsg parsefunction(pcontent,pcomsize,callfn,subtype) #there are 2 more arguments but are not used
+SetCall errormsg parsefunction(pcontent,pcomsize,callfn,subtype) #there is 1 more argument but is not used
