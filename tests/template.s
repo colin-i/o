c790e5708960413ca414cdbf4c7c45787724c78e
tests <costin.b.84@gmail.com>
2023-05-22 19:03:03 +0300

    tests

--- tests/template.s
+++ tests/template.s
@@ -7,19 +7,28 @@ function file()
 	datax a#1
 endfunction
 
-entry main()
+function line()
+	datax a#1
+endfunction
+
+entry main(sd *,sd c)
+sd *#1
+sd n#2
+value *=0x77
 value test#1;value test2#1;data a#1
 sv aux^test
 value auxdata^test
 
-set aux#:file.path 2
-set aux#:file.lines 3
-
 set test:file.a 4
+set test:line.a 4
+set n:main.n 4
+set n:main.c 4
 
 #not this right now
 #set aux#:file.a 4
 set auxdata#:file.a 4
 
+set aux#:file.path 2
+set aux#:file.lines 3
 add test test2
 return test
