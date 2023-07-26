3f0c8fa8bed99fdc8fd3c60c7a2a57703c2b9ed9
bc <costin.botescu@gmail.com>
2023-07-26 09:13:09 +0300

    info and was a char widechar comparison

--- src/windows/obj.s
+++ src/windows/obj.s
@@ -1,7 +1,11 @@
 
 Include "./head.h"
 
-#i686-w64-mingw32-gcc will not argc argv, only x86_64-w64-mingw32-gcc
+#i686-w64-mingw32-gcc has main(argc,argv) but gcc want -8 at inplace instead of 0
 
-Entry _WinMain@16(sd *argc,sd *argv)
+#gcc:
+#	int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
+#but lpCmdLine is a structure, the string is after some bytes
+
+Entry _WinMain@16()
 	Include "./text.s"
