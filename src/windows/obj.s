cb55a35410e14eef297a29d83c04e573d55c9edf
bc <costin.botescu@gmail.com>
2023-07-26 13:08:36 +0300

    info

--- src/windows/obj.s
+++ src/windows/obj.s
@@ -1,7 +1,8 @@
 
 Include "./head.h"
 
-#i686-w64-mingw32-gcc has main(argc,argv) but gcc want -8 at inplace instead of 0
+#gcc(i686/x86_64) will ask for WinMain, but it has argc,argv entry, there are some additional steps
+#old remark: gcc want -8 at inplace instead of 0
 
 #gcc:
 #	int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
