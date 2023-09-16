a599878ccd6c03fba85b8e33a37f6b6a65e218b6
bc <costin.botescu@gmail.com>
2023-09-16 15:25:56 +0300

    x decl sign plan

--- src/files/functions/xfile_details.s
+++ src/files/functions/xfile_details.s
@@ -14,6 +14,23 @@ function xfile_add_declare(ss content,sd size)
 		endelse
 		if err==(noerror)
 			setcall err xfile_add_string(content,size)
+#			if sign!=(sign_not_required)
+#				if err==(noerror)
+#					if sign==(assignsign)
+#						if reloc==(FALSE)
+#						const Xfile_declsign_equal=0
+#						else
+#						const Xfile_declsign_reloc=1
+#						endelse
+#					elseif sign==(reserveascii)
+#					const Xfile_declsign_reserve=2
+#					elseif sign==(pointersigndeclare)
+#					const Xfile_declsign_pointer=3
+#					else
+#					#if sign==(nosign) reserve
+#					endelse
+#				endif
+#			endif
 		endif
 		return err
 	endif
