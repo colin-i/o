e359dc740f4558871099354c900fd53a1957f861
tests <costin.b.84@gmail.com>
2023-10-21 10:04:22 +0300

    test

--- tests/xf/function.s
+++ tests/xf/function.s
@@ -3,3 +3,11 @@ format elfobj
 
 function test(char a,data b,value c,sv d,str e,ss f,vstr g,sd h,vdata *i)
 endfunction
+
+functionx test2()
+endfunction
+
+functionxx test3()
+endfunction
+
+entry test4()
