31da0a0b22dcd411167c3ca7c6140451b372e162
tests <costin.b.84@gmail.com>
2023-11-04 14:49:49 +0200

    "tests"

--- tests/xf/onearg.s
+++ tests/xf/onearg.s
@@ -5,10 +5,12 @@ function b()
 data a#1
 endfunction
 
+entry main()
+
 vstr a="qwer"
 
 return 2
-inc a
+inc #a
 incst a
 dec a
 decst a
@@ -20,3 +22,4 @@ sar a
 exit 3
 return "asdf"
 return a:b.a
+return main.a:b.a
