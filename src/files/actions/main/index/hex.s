42d579bd084f8eb3946464404e3e54decd238c8c
bc <costin.botescu@gmail.com>
2023-12-03 08:17:18 +0200

    hex at xfile

--- src/files/actions/main/index/hex.s
+++ src/files/actions/main/index/hex.s
@@ -1,3 +1,8 @@
 
-
-setcall errormsg enumcommas(pcontent,pcomsize,pcomsize#,(fndecandgroup),(charnumber),(NULL),(hexenum)) #there are 4 more arguments but are not used
+setcall errormsg xfile_add_char_if((Xfile_hex))
+if errormsg=(noerror)
+	setcall errormsg enumcommas(pcontent,pcomsize,pcomsize#,(fndecandgroup),(charnumber),(NULL),(hexenum)) #there are 4 more arguments but are not used
+	if errormsg=(noerror)
+		setcall errormsg xfile_add_commas_if()
+	endif
+endif
