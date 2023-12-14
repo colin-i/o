cb391cbead06bc391f10eb764a5a901fe99ceeb0
bc <costin.botescu@gmail.com>
2023-12-14 07:56:36 +0200

    at otoc, file name arg

--- otoc/otoc.s
+++ otoc/otoc.s
@@ -1,5 +1,14 @@
 
 format elfobj64
 
-entry main()
-return 0
+const EXIT_SUCCESS=0
+const EXIT_FAILURE=1
+
+importx "puts" puts
+
+entry main(sd argc)
+	if argc=2
+		return (EXIT_SUCCESS)
+	end
+	call puts("Usage otoc Xfile name")
+	return (EXIT_FAILURE)
