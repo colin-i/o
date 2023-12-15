7ac74b59e241845a06c19f82ab19333b710693d4
bc <costin.botescu@gmail.com>
2023-12-15 19:56:57 +0200

    no demonstration for that "if"

--- otoc/loop.s
+++ otoc/loop.s
@@ -9,18 +9,17 @@ importx "free" free
 const SEEK_END=2
 
 function loop(sd input)
-	sd a;set a fseek(input,0,(SEEK_END))
+	sd a;set a fseek(input,0,(SEEK_END)) #on 32 can be -1 return error
 	if a=0
-		sd size;set size ftell(input)
-		if size!=-1
-			call rewind(input)
-			sd buffer;set buffer malloc(size)
-			if buffer!=(NULL)
-				sd r;set r fread(buffer,size,1,input)
-				if r=1
-				end
-				call free(buffer)
+		sd size;set size ftell(input) #is still same place if file deleted in parallel
+		#if size!=-1  #lseek and same result (remark fileno)
+		call rewind(input)
+		sd buffer;set buffer malloc(size)
+		if buffer!=(NULL)
+			sd r;set r fread(buffer,size,1,input)
+			if r=1
 			end
+			call free(buffer)
 		end
 	end
 end
