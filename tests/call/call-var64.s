873e3ae48eb1acfb267b565f80e1834562ed3b7e
tests <costin.b.84@gmail.com>
2023-12-22 19:04:14 +0200

    "tests"

--- tests/call/call-var64.s
+++ tests/call/call-var64.s
@@ -3,19 +3,9 @@ format elfobj64
 
 data a#1
 #call a()
-str b#1
-#call b()
 vdata c#1
-call c()
-vstr d#1
-call d()
+#call c()
 value e#1
 call e()
-sd f#1
-call f()
-ss g#1
-call g()
 sv h#1
 call h()
-sw i#1
-call i()
