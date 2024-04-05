7b2f77e58570e6ae85f9c4adc8ad882fc22b8248
bc <costin.botescu@gmail.com>
2024-04-05 10:41:03 +0300

    permissions at least on linux

--- src/linux/files/xheaders.h
+++ src/linux/files/xheaders.h
@@ -8,10 +8,12 @@ Const flag_O_BINARY=0       #this on linux is not a flag
 Const flag_MAX_PATH=PATH_MAX
 
 Const flag_O_CREAT=0x0040
-const S_IRWXU=0x1C0
-const S_IRGRP=0x20;const S_IXGRP=0x8
-const S_IROTH=0x4;const S_IXOTH=0x1
-Const pmode_mode=S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH
+const S_IRWXU=0700
+const S_IRUSR=0400;const S_IWUSR=0200
+const S_IRGRP=0040;const S_IXGRP=0010
+const S_IROTH=0004;const S_IXOTH=0001
+Const exec_perms=S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH
+const nonexec_perms=S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH
 
 Const F_OK=0
 Const X_OK=1
--- src/windows/files/winheaders.h
+++ src/windows/files/winheaders.h
@@ -6,7 +6,8 @@ Const flag_MAX_PATH=260
 
 Const flag_O_CREAT=0x0100
 const _S_IREAD=0x100;const _S_IWRITE=0x80
-Const pmode_mode=_S_IREAD|_S_IWRITE
+const exec_perms=_S_IREAD|_S_IWRITE
+const nonexec_perms=exec_perms
 
 data argv#1
 const p_argv^argv
