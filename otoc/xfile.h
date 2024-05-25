e1644774e4552da470e011ca46aac53b44d2f880
bc <costin.botescu@gmail.com>
2024-05-25 13:13:23 +0300

    can't use ld on amd64 launchpad

--- otoc/xfile.h
+++ otoc/xfile.h
@@ -1,2 +1,4 @@
-einclude "../src/files/headers/xfile.h" "/usr/include/ocompiler/xfile.h"
+
 #einclude? will use all constants in the header. yes, but some are used without touching them, like in loop()
+einclude "/usr/include/ocompiler/xfile.h" "../src/files/headers/xfile.h"
+#        entryraw                         entry
